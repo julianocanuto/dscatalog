baa763e526805a5873ab952626ecec50ae55c3c9
Juliano Canuto <julianocanuto@gmail.com>
2021-09-19 18:45:07 -0300

    Implements resource to insert new category

--- backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
@@ -33,4 +33,12 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundExcepetion("Entity not found"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 }
