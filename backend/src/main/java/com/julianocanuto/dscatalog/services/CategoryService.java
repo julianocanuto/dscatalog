aebbea3d2852111b9fbe629cc81b22061681c521
Juliano Canuto <julianocanuto@gmail.com>
2021-09-19 16:02:17 -0300

    Refactor to use CategoryDTO

--- backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
@@ -1,22 +1,26 @@
 package com.julianocanuto.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.julianocanuto.dscatalog.dto.CategoryDTO;
 import com.julianocanuto.dscatalog.entities.Category;
 import com.julianocanuto.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+
 	}
 }
