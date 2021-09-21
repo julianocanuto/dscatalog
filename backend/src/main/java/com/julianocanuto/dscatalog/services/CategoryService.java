01d02e80312dc3f6db705a02d4b1070862448d74
Juliano Canuto <julianocanuto@gmail.com>
2021-09-21 20:37:43 -0300

    Implements paged findAll to Category

--- backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/julianocanuto/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +31,11 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
 
+	}
+
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(category -> new CategoryDTO(category));
 	}
 
 	@Transactional(readOnly = true)
@@ -54,9 +61,9 @@ public class CategoryService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
-			
+
 		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id " + id + " not found");			
+			throw new ResourceNotFoundException("Id " + id + " not found");
 		}
 	}
 
@@ -65,10 +72,10 @@ public class CategoryService {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id " + id + " not found");
-			
+
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
-		
+
 	}
 }
