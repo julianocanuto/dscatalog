b9158648bcc951999bd7106f8b2ac8ccfc57f96e
Juliano Canuto <julianocanuto@gmail.com>
2021-10-03 21:00:23 -0300

    Refactor endpoint products to use Pageable interface for pagination

--- backend/src/main/java/com/julianocanuto/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/julianocanuto/dscatalog/services/ProductService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,8 +39,8 @@ public class ProductService {
 
 	}
 
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> list = repository.findAll(pageable);
 		return list.map(product -> new ProductDTO(product));
 	}
 
