1dbf56c8fa1bbb3ed804403645ed828da47bbadd
Juliano Canuto <julianocanuto@gmail.com>
2021-10-05 20:58:02 -0300

    Implements test to delete Product when Id does not exist

--- backend/src/test/java/com/julianocanuto/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/julianocanuto/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.julianocanuto.dscatalog.entities.Product;
 
@@ -25,5 +26,15 @@ public class ProductRepositoryTests {
 		
 		Assertions.assertFalse(result.isPresent());
 
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		
+		long nonExistingId = 1000L;
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
 	}
 }
