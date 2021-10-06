bc61b73e8d6a9b57649f918070e942dc1a1c66a1
Juliano Canuto <julianocanuto@gmail.com>
2021-10-05 21:09:11 -0300

    Refactor to use JUnit fixtures and avoid duplicate code

--- backend/src/test/java/com/julianocanuto/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/julianocanuto/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.julianocanuto.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,24 +16,28 @@ public class ProductRepositoryTests {
 
 	@Autowired
 	private ProductRepository repository;
-	
+	private Long existingId;
+	private Long nonExistingId;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 
-		Long existingId = 1L;
-		
 		repository.deleteById(existingId);
 		Optional<Product> result = repository.findById(existingId);
-		
+
 		Assertions.assertFalse(result.isPresent());
 
 	}
-	
+
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
-		
-		long nonExistingId = 1000L;
-		
+
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
 		});
