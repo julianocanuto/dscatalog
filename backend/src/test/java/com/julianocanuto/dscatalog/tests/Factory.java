aab3cbc2e5763fb1ffb8d853a55feb900924b3c2
Juliano Canuto <julianocanuto@gmail.com>
2021-10-10 19:10:20 -0300

    Refactor createProduct to use static method to createCategory

--- backend/src/test/java/com/julianocanuto/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/julianocanuto/dscatalog/tests/Factory.java
@@ -9,7 +9,7 @@ import com.julianocanuto.dscatalog.entities.Product;
 public class Factory {
 	public static Product createProduct() {
 		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
-		product.getCategories().add(new Category(2L, "Electronics"));
+		product.getCategories().add(createCategory());
 		return product;
 	}
 
