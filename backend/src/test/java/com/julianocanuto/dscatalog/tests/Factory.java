a6497e36d4046a003cfbb874fcb663318189de6e
Juliano Canuto <julianocanuto@gmail.com>
2021-10-10 19:08:48 -0300

    Implements static method to create new category

--- backend/src/test/java/com/julianocanuto/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/julianocanuto/dscatalog/tests/Factory.java
@@ -12,9 +12,13 @@ public class Factory {
 		product.getCategories().add(new Category(2L, "Electronics"));
 		return product;
 	}
-	
+
 	public static ProductDTO createProductDTO() {
 		Product product = createProduct();
 		return new ProductDTO(product, product.getCategories());
+	}
+
+	public static Category createCategory() {
+		return new Category(2L, "Electronics");
 	}
 }
