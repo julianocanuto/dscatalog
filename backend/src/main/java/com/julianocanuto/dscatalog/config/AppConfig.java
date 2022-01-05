ba77cea7cdfb827bde722d63b8ab0eb2985b569e
Juliano Canuto <julianocanuto@gmail.com>
2022-01-05 18:53:18 -0300

    Implements @Beans to JWT Token

--- backend/src/main/java/com/julianocanuto/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/julianocanuto/dscatalog/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.julianocanuto.dscatalog.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,17 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
